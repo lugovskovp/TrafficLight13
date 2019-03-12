8980df7cbced7cff395d6219e02971330fedc164
p.lugovskov <p.lugovskov@gmail.com>
2019-03-12 22:43:56 +0300

    add uint16_t vars check for ROM paying

--- TrafficLight13/tst.ino
+++ TrafficLight13/tst.ino
@@ -1,19 +1,22 @@
 #include <avr/io.h>  
 //uint8_t cnt;	//global, .bss
-uint8_t cnt __attribute__ ((section (".noinit")));  //.noinit
+//uint16_t cnt __attribute__ ((section (".noinit")));  //.noinit
 int main(){
-	//uint8_t cnt;	//local
+	uint16_t cnt;	//local
     cnt=0;      // < setup()
     while(1){
         cnt++;  // < loop()
     }
 }
 
-//Program:      58 bytes (5.7% Full) - local
 
-//Program:      62 bytes (6.1% Full) - noinit
+//uint8_t Program:      78 bytes (7.6% Full) - global
+//uint8_t Program:      62 bytes (6.1% Full) - noinit
+//uint8_t Program:      58 bytes (5.7% Full) - local
 
-//Program:      78 bytes (7.6% Full) - global
 
+// uint16_t noinit Program:      74 bytes (7.2% Full)
+// uint16_t local  Program:      64 bytes (6.3% Full)
 
 //"C:\Users\...\Documents\Arduino\TrafficLight13\gcc\0_MAKE & asm-O0.cmd" $(FULL_CURRENT_PATH)
+// add uint16_t vars check for ROM paying
